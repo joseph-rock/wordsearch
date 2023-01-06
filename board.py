0d7e8623a5f06450101d553e711c1e82d7ea3e4e
joseph-rock <josephrock225@gmail.com>
2023-01-06 00:00:28 -0800

    Init

--- board.py
+++ board.py
@@ -17,6 +17,69 @@ def generate_board(dimesion: int) -> List[List[str]]:
         board.append(row)
     return board
 
+def board_rows(board: List[List[str]]) -> List[str]:
+    rows = []
+    for row in board:
+        rows.append(''.join(row))
+    return rows
+
+def board_columns(board: List[List[str]]) -> List[str]:
+    cols = []
+    for i in range(len(board[0])):
+        col = ''
+        for row in board:
+            col += row[i]
+        cols.append(col)
+    return cols
+
+def board_diagonals(board: List[List[str]]) -> List[str]:
+    max = len(board)
+    diagnals = []
+
+    # L to R top row
+    for start in range(max-2):
+        diagnal = ''
+        x = start
+        for row in board:
+            if x >= max:
+                break
+            diagnal += row[x]
+            x += 1
+        diagnals.append(diagnal)
+
+    # L to R left
+    for y in range(1, max-2):
+        diagnal = ''
+        for x in range(0, max):
+            if y >= max:
+                break
+            diagnal += board[y][x]
+            y += 1
+        diagnals.append(diagnal)
+
+    # R to L top row
+    for start in range(2, max):
+        diagnal = ''
+        x = start
+        for row in board:
+            if x < 0:
+                break
+            diagnal += row[x]
+            x -= 1
+        diagnals.append(diagnal)
+
+    # R to L right
+    for y in range(1, max-2):
+        diagnal = ''
+        for x in range(0, max):
+            if y >= max:
+                break
+            diagnal += board[y][max - x - 1]
+            y += 1
+        diagnals.append(diagnal)
+
+    return diagnals
+
 def pretty_print(board: List[List[str]]) -> None:
     for row in board:
         print(*row)
--- main.py
+++ main.py
@@ -2,29 +2,56 @@ from typing import List, Dict
 import word_dict
 import board
 
-
-# print(word_dict.is_word('asdfas ec', indexed_dict))
-# board.pretty_print(crossword)
-
-def foo(char_list: List[str], indexed_dict: Dict) -> List[str]:
-    chars = ''.join(char_list)
+def words_in_string(char_list: List[str], indexed_dict: Dict) -> List[str]:
+    forward = ''.join(char_list)
+    backward = forward[::-1]
+    length = len(forward)
     words = []
 
-    for end_index in range(0, len(chars)+1):
-        check_str = chars[0: end_index]
-        if word_dict.is_word(check_str, indexed_dict):
-            words.append(check_str)
+    for start_index in range(0, length):
+        for end_index in range(start_index + 2, length):
+            check_forward = forward[start_index: end_index + 1]
+            if word_dict.is_word(check_forward, indexed_dict):
+                words.append(check_forward)
+
+            check_backward = backward[start_index: end_index + 1]
+            if word_dict.is_word(check_backward, indexed_dict):
+                words.append(check_backward)
 
     return words
 
 
 def main():
+    size = 100
     indexed_dict = word_dict.indexed_dict()
-    crossword = board.generate_board(50)
+    gameboard = board.generate_board(size)
 
-    word = "cannotasdflkjsdckjvnskjdfkljsdflksjdflksjdflksndclkjsndlkfjslkgjslkdflskdjflskdjcskjldhnfglisjdg".split()
+    rows = board.board_rows(gameboard)
+    cols = board.board_columns(gameboard)
+    diags = board.board_diagonals(gameboard)
 
-    print(foo(word, indexed_dict))
+    # board.pretty_print(gameboard)
+    print("Load Complete")
+    
+    words = []
+    for row in rows:
+        words += words_in_string(row, indexed_dict)
+    print("Rows complete")
+    for col in cols:
+        words += words_in_string(col, indexed_dict)
+    print("Cols complete")
+    for diag in diags:
+        words += words_in_string(diag, indexed_dict)
+    print("Diagnals complete")
+    print()
+    
+    words = list(set(words))
+    words.sort()
+    print(*words)
+
+    # for word in words:
+    #     if len(word) > 3:
+    #         print(word)
 
 
 if __name__ == "__main__":
